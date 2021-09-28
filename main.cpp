91d5d2b266df896fce92fd6d1ce088402ca3465c
Алексей <frstfng@gmail.com>
2021-09-28 22:32:54 +0300

    2 left

--- main.cpp
+++ main.cpp
@@ -22,16 +22,17 @@ private:
     class Node{
     public:
         Node* pNextNode;
+        Node* pPrevNode;
         T1 data;
-        Node (T1 data = T1(), Node* pNext=nullptr){
+
+        Node (T1 data = T1(), Node* pNext=nullptr, Node* pPrev=nullptr) {
             this->data = data;
             this->pNextNode = pNext;
+            this->pPrevNode = pPrev;
         }
     private:
-
     };
-
-    Node<T> *head;
+    Node<T>* head;
     int size;
 };
 
@@ -43,14 +44,14 @@ List<T>::List(){
 
 template<typename T>
 List<T>::~List(){
-    this->Clear();
 }
 
 template<typename T>
 void List<T>::Pop_front()
 {
-    Node<T> *tmp = head;
-    this->head = head->pNextNode;
+    Node<T>* tmp = this->head;
+    this->head = tmp->pNextNode;
+    this->head->pPrevNode = nullptr;
     delete tmp;
     this->size--;
 }
@@ -58,144 +59,108 @@ void List<T>::Pop_front()
 template<typename T>
 void List<T>::Push_back(T data)
 {
-    if(head ==nullptr){
-        head = new Node<T>(data);
+    if (this->head == nullptr) {
+        this->head = new Node<T>(data);
+        this->size++;
     }
-    else{
-        Node<T> * current = this->head;
-        while(current->pNextNode != nullptr){
-            current = current->pNextNode;
+    else if (this->head != nullptr) {
+        Node<T>* tmp = this->head;
+        while (tmp->pNextNode != nullptr) {
+            tmp = tmp->pNextNode;
         }
-        current->pNextNode = new Node<T>(data);
+        tmp->pNextNode = new Node<T>(data, nullptr, tmp);
+        this->size++;
     }
-    this->size++;
 }
 
 template<typename T>
 void List<T>::Show_list()
 {
-    for(int i = 0; i < this->size; i++){
-        cout << this->operator[](i) << " ";
+    cout << "=========================\n";
+    Node<T>* tmp = this->head;
+    for (int i = 0; i < this->size; i++) {
+        cout << "this = " << tmp << endl;
+        cout << "prev = " << tmp->pPrevNode << endl;
+        cout << "next = " << tmp->pNextNode << endl;
+        cout << "data = " << tmp->data << endl;
+        cout << "-----" << endl;
+        tmp = tmp->pNextNode;
     }
-    cout << endl;
-
-    cout << this->size << " elements in list" << endl;
 }
 
 template<typename T>
 void List<T>::Clear()
 {
-    while (size) {
-        Pop_front();
+    Node<T>* tmp = this->head;
+    while(this->head != nullptr){
+        tmp = this->head;
+        this->head = tmp->pNextNode;
+        delete tmp;
+        this->size--;
     }
 }
 
 template<typename T>
 void List<T>::Push_Front(T data)
 {
-    this->head = new Node<T>(data, this->head);
+    this->head = new Node<T>(data, this->head, nullptr);
+    this->head->pNextNode->pPrevNode = this->head;
     this->size++;
 }
 
 template<typename T>
 void List<T>::Pop_Back()
 {
-    Node<T>* tmp = this->head;
-    while (tmp->pNextNode != nullptr) {
-        tmp = tmp->pNextNode;
+    Node<T>* next = this->head;
+    for(int i = 0; i < this->size - 1; i++){
+        next = next->pNextNode;
     }
-
-    Node<T>* previous = this->head;
-    for (int i = 0; i < this->size - 1; i++) {
-        previous = previous->pNextNode;
+    Node<T>* prev = this->head;
+    for(int i = 0; i < this->size - 2; i++){
+        prev = prev->pNextNode;
     }
-
-    previous->pNextNode = nullptr;
-    delete tmp;
+    prev->pNextNode = nullptr;
+    delete next;
     this->size--;
 }
 
 template<typename T>
 int List<T>::insert(T data, int index)
 {
-    if (index < 0) {
+    if(index <= 0){
         return 1;
     }
-    else if (index == 0) {
+    else if(index == 1){
         this->Push_Front(data);
     }
-    else if (index == this->size) {
-        this->Push_back(data);
-    }
-    else if (index > 0 && index < this->size) {
-        Node<T>* previous = this->head;
-
-        for (int i = 0; i < index - 1; i++) {
-            previous = previous->pNextNode;
+    else if(index > 1 && index <= this->size){
+        Node<T>* prevNode = this->head;
+        for(int i = 0; i < index - 2; i++){
+            prevNode = prevNode->pNextNode;
         }
-
-        Node<T>* newNode = new Node<T>(data, previous->pNextNode);
-        previous->pNextNode = newNode;
+        Node<T>* nextNode = prevNode->pNextNode;
+        Node<T>* tmp = new Node<T>(data, nextNode, prevNode);
+        prevNode->pNextNode = tmp;
+        nextNode->pPrevNode = tmp;
         this->size++;
     }
-    return 0;
-}
-
-template<typename T>
-int List<T>::removeAt(int index)
-{
-    if (index < 0) {
-        return 1;
-    }
-    else if (index == 0) {
-        this->Pop_Back();
-        return 0;
-    }
-    else if (index > 0 && index < this->size) {
-        Node<T>* previous = this->head;
-        for (int i = 0; i < index - 1; i++) {
-            previous = previous->pNextNode;
-        }
-        Node<T>* toDelete = previous->pNextNode;
-        previous->pNextNode = toDelete->pNextNode;
-        delete toDelete;
-        this->size--;
-        return 0;
-    }
-    else if (index == this->size) {
-        this->Pop_front();
-        return 0;
+    else if(index > this->size){
+        this->Push_back(data);
     }
     return 0;
-}
-
-template<typename T>
-T &List<T>::operator[](const int index)
-{
-    int counter = 0;
-    Node<T> *current = this->head;
-    while(current != nullptr){
-        if(counter == index){
-            return current->data;
-        }
-        current = current->pNextNode;
-        counter++;
-    }
 }
 
 int main()
 {
     List<int> lst;
 
-    lst.Push_Front(999);
-    lst.Push_back(2);
-    lst.Push_back(10);
-    lst.Push_back(22);
-    lst.Push_back(34);
+    lst.Push_back(29);
     lst.Push_back(56);
+    lst.Push_back(3);
+    lst.Push_back(10);
+    lst.Push_back(59);
+    lst.insert(844, 0);
+    lst.Show_list();
 
-    lst.Show_list();
-    lst.removeAt(3);
-    lst.Show_list();
     return 0;
 }
